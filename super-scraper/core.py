3b3eb46ecec8905f538a4e7694536894cb697282
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2023-01-23 13:09:43 +0545

    feat: extract.py

--- super-scraper/core.py
+++ super-scraper/core.py
@@ -2,52 +2,55 @@ from exceptions import RequestException
 from bs4 import BeautifulSoup
 from urllib3 import PoolManager
 from urllib3.response import HTTPResponse
-import urllib3 
 
 
 class __HTTPPool:
     """Private class to create PoolManager instance."""
 
     def __init__(self):
-        self.manager = self.create_pool_manager()
+        self.manager = self.__create_pool_manager()
 
-    def create_pool_manager(self) -> PoolManager:
-        return urllib3.PoolManager()
+    def __create_pool_manager(self) -> PoolManager:
+        return PoolManager()
 
 
 class HTTPRequestResponseHandler(__HTTPPool):
     """This class is responsible for request and response handling"""
 
-    def response(self, url: str) -> HTTPResponse:
+    def __init__(self, url: str) -> None:
+        """
+        Constructor of HTTPRequestResponseHandler class
+
+        Parameters: 
+            url (str) : The url string to send the request to
+        """
+        self.url = url
+        super().__init__()
+
+    def response(self) -> HTTPResponse:
         """
         The function to get the response of given url.
-
-        Parameters:
-            url (str) : The url string to send the request to.
 
         Returns: 
             HTTPResponse: A response object of the given url endpoint.
         """
-        response = self.manager.request("GET", url)
-        if response.status is 404:
+        response = self.manager.request("GET", self.url)
+        if response.status == 404:
             raise RequestException("Invalid url")
         return response
 
-    def get_html(self, url:str) -> bytes:
+    def get_html(self) -> bytes:
         """
         The function to retrieve HTML document of the given url.
 
-        Parameters:
-            url (str) : The url string to send the request to.
-        
         Returns: 
             html (bytes) : bytes object form of html document.
         """
 
-        html = self.response(url)._body
+        html = self.response()._body
         return html
     
-    def prettify_html(self, html:bytes) -> str:
+    def prettify_html(self, html: bytes) -> str:
         """
         The function to Prettify the html document.
 
@@ -62,4 +65,5 @@ class HTTPRequestResponseHandler(__HTTPPool):
 
         prettified_html = soup.prettify();
         return prettified_html
+
 
---
+++ super-scraper/extract.py
@@ -0,0 +1,29 @@
+from scraper import HTTPRequestResponseHandler
+from bs4 import BeautifulSoup
+
+handler = HTTPRequestResponseHandler("https://stackoverflow.com/questions/12117087/python-hide-methods-with")
+
+def extract_meta():
+    """
+    The function to extract meta tag.
+
+    Returns:   
+        meta_list (list) : List of
+    """
+
+    handler.response()
+    html = handler.get_html()
+    soup = BeautifulSoup(html, 'html.parser')
+    
+    meta_list = [] 
+    for tag in soup.find_all("meta"):
+        one_list = [] 
+        one_list.append(tag.get("name", ""))
+        one_list.append(tag.get("content", ""))
+        meta_list.append(one_list)
+
+    return meta_list
+
+
+
+
